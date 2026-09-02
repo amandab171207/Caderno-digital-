9ba6db26dde9af0e9b36a8c38cd084812fa5f1aa
amandab171207 <amandab171207@gmail.com>
2026-09-02 18:51:20 -0300

    Remover os três cadernos indesejados da sincronização

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='caderno-digital-v89';
+const CACHE_NAME='caderno-digital-v90';
 const APP_FILES=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icons/app-icon.svg','./icons/app-icon-192.png','./icons/app-icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
